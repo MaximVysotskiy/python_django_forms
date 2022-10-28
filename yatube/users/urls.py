59d90cde3f0df5bfc641509fa7e62e13b8fdc1df
Максим Высоцкий <q79532925360@yandex.ru>
2022-10-28 13:23:13 +0300

    yatube декораторы

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -29,31 +29,32 @@ urlpatterns = [
     ),
     path(
         'password_change/',
-        PasswordChangeView.as_view (template_name='users/password_change_form.html'),
+        PasswordChangeView.as_view(template_name= \
+            'users/password_change_form.html'),
         name='password_change'
     ),
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view(template_name=
-        'users/password_change_done.html'),
+        PasswordChangeDoneView.as_view(template_name= \
+            'users/password_change_done.html'),
         name='password_change_done'
     ),
     path(
         'reset/done/',
-        PasswordResetCompleteView.as_view(template_name=
-        'users/password_reset_complete.html'),
+        PasswordResetCompleteView.as_view(template_name= \
+            'users/password_reset_complete.html'),
         name='password_reset_complete'
     ),
     path(
         'reset/<uidb64>/<token>',
-        PasswordResetConfirmView.as_view(template_name=
-        'users/password_reset_confirm.html'),
+        PasswordResetConfirmView.as_view(template_name= \
+            'users/password_reset_confirm.html'),
         name='password_reset_confirm'
     ),
     path(
         'password_reset/done/',
-        PasswordResetDoneView.as_view(template_name=
-        'users/password_reset_done.html'),
+        PasswordResetDoneView.as_view(template_name= \
+            'users/password_reset_done.html'),
         name='password_reset_done'
     ),
 ]
